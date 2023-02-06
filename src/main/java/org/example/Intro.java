4a64fe825c354bdf4efff31bdb973794f82d83c9
finkw <fink.werner290@gmail.com>
2023-02-06 20:57:34 +0200

    Intro - Commiy 2

--- src/main/java/org/example/Intro.java
+++ src/main/java/org/example/Intro.java
@@ -1,9 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
 public class Intro {
     public static void main(String[] args) {
 
+        /*
         System.out.println("Hello");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        String name = input.nextLine();
+        System.out.println("Welcome to hte Java course, " + name);
+
+        //Output Text:
+        /*
+        Multiple line comment
+        */
+        System.out.println("Text with a qoute \"quote\");
+        System.out.println("The project is in C:\\Users");
+        System.out.println("Text with blank space:  and other text after");
+        System.out.println("Text ist text \n on \n multiple \n lines");
 }
 }
 
